67bf8baec6345419198ce5d2a1f78366cc87c7bb
dishu001 <2020eeb1167@iitrpr.ac.in>
2023-06-03 11:57:37 +0530

    change in home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,6 +6,7 @@ import Footer from "../../components/Footer";
 // import HomeContent from "../../components/HomeContent"
 
 import { TypeAnimation } from "react-type-animation";
+import { items, services_home } from "./constants";
 
 const Home = () => {
   const [textColor, setTextColor] = useState("black");
@@ -29,21 +30,6 @@ const Home = () => {
 
   useEffect(() => {
     const colorInterval = setInterval(() => {
-      const items = [
-        "aqua",
-        "deeppink",
-        "darkkhaki",
-        "blue",
-        "green",
-        "purple",
-        "pink",
-        "brown",
-        "darkmagenta",
-        "darksalmon",
-        "dodgerblue",
-        "firebrick",
-        "darkviolet",
-      ];
       const randomColor = items[Math.floor(Math.random() * items.length)];
       setTextColor(randomColor);
     }, 1000); // Change color every 3 seconds
@@ -93,7 +79,7 @@ const Home = () => {
         <div className="text-lg font-bold mb-2">
           Discover the best coding course for your kids.
         </div>
-        <div className="text-base">Learn Coding from basics.</div>
+        <div className="text-base ml">Learn Coding from basics.</div>
         <div className="btn-div my-5">
           <Link
             to="/dashboard/courses"
@@ -113,64 +99,39 @@ const Home = () => {
         </div>
       </div>
 
-      <section className="py-8 bg-gray-100">
+      <section className="py-8 bg-gray-100 pb-16">
         <div className="container mx-auto px-4">
-          <h2 className="text-3xl text-center font-bold mb-8">WHY CHOOSE 
-          <span className="text-3xl  font-bold text-blue-600">
-              {" "}US?
-          </span>
+          <h2 className="text-3xl text-center font-bold mb-14">
+            WHY CHOOSE
+            <span className="text-3xl  font-bold text-blue-600"> US?</span>
           </h2>
-          <div className="relative h-2 mb-6 overflow-hidden">
+          <div className="relative h-2 mb-6 overflow-hidden m-4">
             <div
-              className="absolute h-full w-full bg-blue-400"
+              className="absolute h-full w-full bg-blue-400 rounded-3xl"
               style={{ width: `${scrollPercentage}%` }}
             ></div>
           </div>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
-            <div className="px-4">
-              <div className="bg-white rounded-lg shadow-lg p-6">
-                <h3 className="text-xl font-bold mb-4">Time Management</h3>
-                <p className="text-gray-600">
-                  We aim for providing hiring solutions for educational
-                  institutions. We recognize the importance of efficient and effective recruitment processes to ensure the smooth functioning of educational institutions. That's why we offer comprehensive hiring solutions designed to streamline the recruitment process and save valuable time.
-                </p>
-              </div>
-            </div>
-            <div className="px-4">
-              <div className="bg-white rounded-lg shadow-lg p-6">
-                <h3 className="text-xl font-bold mb-4">Best Quality Services</h3>
-                <p className="text-gray-600">
-                  Our faculties are a team of highly qualified and extensively
-                  trained professionals. They have graduated from some of the
-                  most prestigious institutions around the world, which is a
-                  testament to their knowledge and expertise.They bring a wealth of experience and, creating a dynamic and engaging learning environment for our students.
-                </p>
-              </div>
-            </div>
-            <div className="px-4">
-              <div className="bg-white rounded-lg shadow-lg p-6">
-                <h3 className="text-xl font-bold mb-4">Practical Learning</h3>
-                <p className="text-gray-600">
-                  We emphasize practical learning through hands-on projects to
-                  enhance students' coding skills.They learn how to analyze requirements and implement them using programming languages.
-                  We believe that practical learning not only enhances technical competence but also nurtures critical thinking, creativity, and teamwork.
-                </p>
-              </div>
-            </div>
-            <div className="px-4">
-              <div className="bg-white rounded-lg shadow-lg p-6">
-                <h3 className="text-xl font-bold mb-4">Affordable Cost</h3>
-                <p className="text-gray-600">
-                  We understand the importance of affordability and
-                  accessibility. Therefore, we offer our services at economical
-                  rates without compromising on quality.Join us on our mission to make coding education accessible and affordable for all, and together, let's unlock the boundless possibilities that coding offers.
-                </p>
-              </div>
-            </div>
+            {services_home &&
+              services_home?.map((service, index) => {
+                return (
+                  <div
+                    className="px-4 user-select-none cursor-pointer"
+                    key={index}
+                  >
+                    <div className="bg-gray-800 hover:bg-white rounded-lg shadow-lg p-6 text-gray-300 hover:text-gray-600">
+                      <h3 className="text-xl font-bold mb-4 text-blue-400 hover:text-blue-600 flex align-items-center align-middle">
+                        <span className="mr-2 mb-1">{service.child}</span>
+                        {service.title}
+                      </h3>
+                      <p className="">{service.description}</p>
+                    </div>
+                  </div>
+                );
+              })}
           </div>
         </div>
       </section>
-
       <div className="mt-1">
         <Footer />
       </div>
---
+++ src/pages/home/constants.jsx
@@ -0,0 +1,50 @@
+import {
+  ClockCircleFilled,
+  CustomerServiceFilled,
+  BookFilled,
+  MoneyCollectFilled,
+} from "@ant-design/icons";
+
+export const items = [
+  "aqua",
+  "deeppink",
+  "darkkhaki",
+  "blue",
+  "green",
+  "purple",
+  "pink",
+  "brown",
+  "darkmagenta",
+  "darksalmon",
+  "dodgerblue",
+  "firebrick",
+  "darkviolet",
+];
+
+export const services_home = [
+  {
+    title: "Time Management",
+    child: <ClockCircleFilled />,
+    description:
+      "We aim for providing hiring solutions for educational institutions. We recognize the importance of efficient and effective recruitment processes to ensure the smooth functioning of educational institutions. That's why we offer comprehensive hiring solutions designed to streamline the recruitment process and save valuable time.",
+  },
+  {
+    title: "Best Quality Services",
+    child: <CustomerServiceFilled />,
+
+    description:
+      "Our faculties are a team of highly qualified and extensively trained professionals. They have graduated from some of the most prestigious institutions around the world, which is a testament to their knowledge and expertise.They bring a wealth of experience and, creating a dynamic and engaging learning environment for our students.",
+  },
+  {
+    title: "Practical Learning",
+    child: <BookFilled />,
+    description:
+      "We emphasize practical learning through hands-on projects to enhance students' coding skills.They learn how to analyze requirements and implement them using programming languages. We believe that practical learning not only enhances technical competence but also nurtures critical thinking, creativity, and teamwork.",
+  },
+  {
+    title: "Affordable Cost",
+    child: <MoneyCollectFilled />,
+    description:
+      "We understand the importance of affordability and accessibility. Therefore, we offer our services at economical rates without compromising on quality.Join us on our mission to make coding education accessible and affordable for all, and together, let's unlock the boundless possibilities that coding offers.",
+  },
+];
